9ccb472498b25a29ec82d841dcc9bdd433acf718
tmatis <tmatis@student.42.fr>
2023-10-11 12:54:36 +0200

    ✨ log syscall num if unknown

--- srcs/syscall/syscall_get_description.c
+++ srcs/syscall/syscall_get_description.c
@@ -1,5 +1,6 @@
 #include "syscall_32.h"
 #include "syscall_64.h"
+#include <ft_printf.h>
 #include <syscall_strace.h>
 
 /**
@@ -10,11 +11,15 @@
  */
 const syscall_description_t *syscall_get_description(uint64_t syscall_no, register_type_t type)
 {
-	static const syscall_description_t default_syscall = {
-		"unknown", INT, {NONE, NONE, NONE, NONE, NONE, NONE}};
+	static char default_syscall_name[32] = {0};
+	static syscall_description_t default_syscall = {
+		default_syscall_name, INT, {NONE, NONE, NONE, NONE, NONE, NONE}};
 	// check if in bounds
 	if (syscall_no >= ELEM_COUNT(x86_64_syscalls))
+	{
+		ft_snprintf(default_syscall_name, sizeof(default_syscall_name), "unknown_%llu", syscall_no);
 		return &default_syscall;
+	}
 	if (type == X86_64)
 		return &x86_64_syscalls[syscall_no];
 	return &x86_32_syscalls[syscall_no];
